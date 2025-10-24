15ad20349f2433e4fa0c3b82b5b27cfb0eb8af93
salim928 <salimadams49@gmail.com>
2025-10-24 22:16:41 +0000

    Simplify scroll lock to prevent flash when closing menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -49,16 +49,11 @@ export default function Header() {
   // Prevent body scroll when mobile menu is open
   useEffect(() => {
     if (mobileMenuOpen) {
-      const scrollY = window.scrollY
-      document.body.style.position = 'fixed'
-      document.body.style.top = `-${scrollY}px`
-      document.body.style.width = '100%'
+      document.body.style.overflow = 'hidden'
+      document.body.style.touchAction = 'none'
     } else {
-      const scrollY = document.body.style.top
-      document.body.style.position = ''
-      document.body.style.top = ''
-      document.body.style.width = ''
-      window.scrollTo(0, parseInt(scrollY || '0') * -1)
+      document.body.style.overflow = ''
+      document.body.style.touchAction = ''
     }
   }, [mobileMenuOpen])
 
